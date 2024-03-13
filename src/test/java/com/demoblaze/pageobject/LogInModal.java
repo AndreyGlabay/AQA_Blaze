777d580a28d9c324d70b9510f437876f07f28312
Andriy Glabay <rishelevsky@gmail.com>
2024-03-13 21:05:51 +0200

    Project: create LogInModal.java page object (copy SignUpModal.java page object)

---
+++ src/test/java/com/demoblaze/pageobject/LogInModal.java
@@ -0,0 +1,11 @@
+package com.demoblaze.pageobject;
+
+public class SignUpModal {
+    public static final String SIGNUP_MODAL_LOCATOR = "//*[@id=\"signInModal\"]/div";
+    public static final String SIGNUP_TITLE_LOCATOR = "//*[@id=\"signInModalLabel\"]";
+    public static final String SIGNUP_X_LOCATOR = "//*[@id=\"signInModal\"]/div/div/div[1]/button/span";
+    public static final String SIGNUP_USERNAME_LOCATOR = "//*[@id=\"sign-username\"]";
+    public static final String SIGNUP_PASSWORD_LOCATOR = "//*[@id=\"sign-password\"]";
+    public static final String SIGNUP_BUTTON_LOCATOR = "//*[@id=\"signInModal\"]/div/div/div[3]/button[2]";
+    public static final String SIGNUP_CLOSE_LOCATOR = "//*[@id=\"signInModal\"]/div/div/div[3]/button[1]";
+}
