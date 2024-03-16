fa64f5b32fdb73708809088ef6e3cf306543b35a
Andriy Glabay <rishelevsky@gmail.com>
2024-03-17 01:03:22 +0200

    Create test class TestData.java and store there usernames and passwords values

--- src/test/java/com/demoblaze/testdata/TestData.java
+++ src/test/java/com/demoblaze/testdata/TestData.java
@@ -1,2 +1,26 @@
-package com.demoblaze.testdata;public class TestData {
+package com.demoblaze.testdata;
+
+public class TestData {
+    public static final String USERNAME = "JohnJohnson";
+    public static final String USERNAME_1 = "timhce4eNP"; // Generated with www.random.org
+    public static final String USERNAME_2 = "x81DfDaelO"; // Generated with www.random.org
+    public static final String USERNAME_3 = "WQ8T1HyUUD"; // Generated with www.random.org
+    public static final String USERNAME_4 = "ESpc8dds9Y"; // Generated with www.random.org
+    public static final String USERNAME_5 = "kWFUYNaAlc"; // Generated with www.random.org
+
+    public static final String PASSWORD = "Test-12345";
+    public static final String PASSWORD_1 = "Test-123abc";
+    public static final String PASSWORD_2 = "Test-abcde";
+    public static final String PASSWORD_3 = "123581321";
+    public static final String PASSWORD_4 = "R0S0T8wZHc"; // Generated with www.random.org
+    public static final String PASSWORD_5 = "vIq3YCdNxi"; // Generated with www.random.org
+
+    public static String uniqueUsernameGC;
+    public static String getUniqueUsernameGC() {
+        return uniqueUsernameGC;
+    }
+    public static void setUniqueUsernameGC(String username) {
+        uniqueUsernameGC = username;
+    }
+
 }
