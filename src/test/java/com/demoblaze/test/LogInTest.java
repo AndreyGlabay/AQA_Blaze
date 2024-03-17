a29ec5ab5a1d00a051b023c13336476d8298cd8b
Andriy Glabay <rishelevsky@gmail.com>
2024-03-17 21:34:15 +0200

    LogInTest.java: (1) Add TC-102 for check that the text in the web element Welcome Button is matched with the pattern; (2) Add TC-103 Add TC-103 which check successful log out procedure, and the web elements presence on the home page after logged out

--- src/test/java/com/demoblaze/test/LogInTest.java
+++ src/test/java/com/demoblaze/test/LogInTest.java
@@ -18,6 +18,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -35,6 +36,7 @@ import static com.demoblaze.pageobject.LogInModal.LOGIN_BUTTON_LOCATOR;
 import static com.demoblaze.pageobject.LogInModal.LOGIN_CLOSE_LOCATOR;
 import static com.demoblaze.pageobject.NavBarLoggedUser.LOGOUT_BUTTON_LOCATOR;
 import static com.demoblaze.pageobject.NavBarLoggedUser.NAVBAR_LOGGED_USER_LOCATOR;
+import static com.demoblaze.pageobject.NavBarLoggedUser.WELCOME_BUTTON_LOCATOR;
 import static com.demoblaze.pageobject.SignUpModal.SIGNUP_BUTTON_LOCATOR;
 import static com.demoblaze.pageobject.SignUpModal.SIGNUP_MODAL_LOCATOR;
 import static com.demoblaze.pageobject.SignUpModal.SIGNUP_PASSWORD_LOCATOR;
@@ -48,21 +50,27 @@ public class LogInTest {
     public String baseUrl = "https://demoblaze.com/";
     SignUpTest signUp = new SignUpTest();
 
-    @BeforeMethod
+    @BeforeClass
     public void signUpInstance() throws MalformedURLException, InterruptedException {
         signUp.successSignUpChrome();                               // Call the signup method before each test method
+
+    }
+
+    @BeforeMethod
+    public void logInInstance() throws MalformedURLException {
         ChromeOptions options = new ChromeOptions();                // New instance for GC browser;
         driver = new RemoteWebDriver(new URL(gridUrl), options);    // Driver initialization on SeleniumGrid using GC;
         driver.get(baseUrl);                                        // Open web page by URL;
     }
+
     @AfterMethod
     public void stopInstance() throws InterruptedException {
         Thread.sleep(3000);
         driver.quit();
     }
 
-    @Test(testName = "TC101_Success_LogIn_CheckElements")
-    public void successLogIn() {
+    @Test(testName = "TC101_Success_LogIn_Check_Elements", priority = 1)
+    public void successLogInCheckElements() {
         HomePage homePage = new HomePage(driver);                           // Create an instance of Home page;
         WebElement navButtonLogIn = homePage.getNavButtonLogIn();           // Get the Login button from the Home page;
         navButtonLogIn.click();                                             // Click on the Login button.
@@ -71,10 +79,10 @@ public class LogInTest {
         WebElement logInModal = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LOGIN_MODAL_LOCATOR)));
 
         WebElement usernameInput = logInModal.findElement(By.xpath(LOGIN_USERNAME_LOCATOR)); // Find the Username input;
-        String uniqueUsernameGC = TestData.getUniqueUsernameGC();                        // Get Username from Test Data;
-        usernameInput.sendKeys(uniqueUsernameGC);                                        // Send the username to the input.
+        String uniqueUsernameGC = TestData.getUniqueUsernameGC();                       // Get Username from Test Data;
+        usernameInput.sendKeys(uniqueUsernameGC);                                       // Send the username to the input.
         WebElement passwordInput = logInModal.findElement(By.xpath(LOGIN_PASSWORD_LOCATOR)); // Find the Password input;
-        passwordInput.sendKeys(TestData.PASSWORD);                                     // Send the password to the input.
+        passwordInput.sendKeys(TestData.PASSWORD);                                      // Send the password to the input.
         WebElement logInButton = logInModal.findElement(By.xpath(LOGIN_BUTTON_LOCATOR)); // Find the LogIn button;
         logInButton.click();                                                             // Click on the button.
 
@@ -84,7 +92,7 @@ public class LogInTest {
         System.out.println();
 
         HomePageLoggedIn homePageLoggedIn = new HomePageLoggedIn(driver); // Initialize the object for logged-in home page;
-        NavBarLoggedUser navBarLoggedUser = homePageLoggedIn.getNavBarLoggedUser(driver); // Take Nav Bar from the logged-in page.
+        NavBarLoggedUser navBarLoggedUser = homePageLoggedIn.getNavBarLoggedUser(driver); // Get NavBar from the page.
 
         SoftAssert softAssert = new SoftAssert(); // Add Soft Assertion (too many assertions);
         // Check the presence of the Nav Bar and its elements:
@@ -100,10 +108,10 @@ public class LogInTest {
         softAssert.assertTrue(homePageLoggedIn.isHeaderLogoDisplayed(), "Carousel is missing");
         softAssert.assertTrue(homePageLoggedIn.isCarouselDisplayed(), "Carousel is missing");
         softAssert.assertTrue(homePageLoggedIn.isCategoryMenuAreaDisplayed(), "Category Menu Area is missing");
-        softAssert.assertTrue(homePageLoggedIn.isCategoryMenuTitleDisplayed(), "Category Menu Title is missing");
-        softAssert.assertTrue(homePageLoggedIn.isCategoryMenuPhonesDisplayed(), "Category Menu Phones is missing");
-        softAssert.assertTrue(homePageLoggedIn.isCategoryMenuLaptopsDisplayed(), "Category Menu Laptops is missing");
-        softAssert.assertTrue(homePageLoggedIn.isCategoryMenuMonitorsDisplayed(), "Category Menu Monitors is missing");
+        softAssert.assertTrue(homePageLoggedIn.isCategoryMenuTitleDisplayed(), "Category Title is missing");
+        softAssert.assertTrue(homePageLoggedIn.isCategoryMenuPhonesDisplayed(), "Category Phones is missing");
+        softAssert.assertTrue(homePageLoggedIn.isCategoryMenuLaptopsDisplayed(), "Category Laptops is missing");
+        softAssert.assertTrue(homePageLoggedIn.isCategoryMenuMonitorsDisplayed(), "Category Monitors is missing");
         softAssert.assertTrue(homePageLoggedIn.isContentAreaDisplayed(), "Content Area is missing");
         softAssert.assertTrue(homePageLoggedIn.isPreviousButtonDisplayed(), "Previous Button is missing");
         softAssert.assertTrue(homePageLoggedIn.isNextButtonDisplayed(), "Next Button is missing");
@@ -113,4 +121,76 @@ public class LogInTest {
         softAssert.assertTrue(homePageLoggedIn.isFooterCopyrightDisplayed(), "Footer Copyright is missing");
         softAssert.assertAll();
     }
+
+
+    @Test(testName = "TC102_Success_LogIn_Check_Username", priority = 2)
+    public void successLogInCheckUsername() {
+        HomePage homePage = new HomePage(driver);                           // Create an instance of Home page;
+        WebElement navButtonLogIn = homePage.getNavButtonLogIn();           // Get the Login button from the Home page;
+        navButtonLogIn.click();                                             // Click on the Login button.
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(10000)); // Initialize WebDriverWait object;
+        WebElement logInModal = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LOGIN_MODAL_LOCATOR)));
+
+        WebElement usernameInput = logInModal.findElement(By.xpath(LOGIN_USERNAME_LOCATOR)); // Find the Username input;
+        String uniqueUsernameGC = TestData.getUniqueUsernameGC();                       // Get Username from Test Data;
+        usernameInput.sendKeys(uniqueUsernameGC);                                       // Send the username to the input.
+        WebElement passwordInput = logInModal.findElement(By.xpath(LOGIN_PASSWORD_LOCATOR)); // Find the Password input;
+        passwordInput.sendKeys(TestData.PASSWORD);                                      // Send the password to the input.
+        WebElement logInButton = logInModal.findElement(By.xpath(LOGIN_BUTTON_LOCATOR)); // Find the LogIn button;
+        logInButton.click();                                                             // Click on the button.
+
+        System.out.println("LOGGED IN WITH CREDENTIALS:");
+        System.out.println("  Logged with Username: " + TestData.getUniqueUsernameGC());
+        System.out.println("  Logged with Password: " + PASSWORD);
+        System.out.println();
+
+        // Check the text in the web element Welcome Button is matched with the pattern "Welcome (username)"
+        WebElement welcomeButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(WELCOME_BUTTON_LOCATOR)));
+        String expectedText = TestData.getUniqueUsernameGC();
+        String actualText = welcomeButton.getText();
+        Assert.assertEquals(actualText, "Welcome " + expectedText, "Welcome Button: wrong username");
+    }
+
+    @Test(testName = "TC103_Success_LogOut_Check_Elements", priority = 2)
+    public void successLogOutCheckElements() {
+        HomePage homePage = new HomePage(driver);                           // Create an instance of Home page;
+        WebElement navButtonLogIn = homePage.getNavButtonLogIn();           // Get the Login button from the Home page;
+        navButtonLogIn.click();                                             // Click on the Login button.
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(10000)); // Initialize WebDriverWait object;
+        WebElement logInModal = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LOGIN_MODAL_LOCATOR)));
+
+        WebElement usernameInput = logInModal.findElement(By.xpath(LOGIN_USERNAME_LOCATOR)); // Find the Username input;
+        String uniqueUsernameGC = TestData.getUniqueUsernameGC();                       // Get Username from Test Data;
+        usernameInput.sendKeys(uniqueUsernameGC);                                       // Send the username to the input.
+        WebElement passwordInput = logInModal.findElement(By.xpath(LOGIN_PASSWORD_LOCATOR)); // Find the Password input;
+        passwordInput.sendKeys(TestData.PASSWORD);                                      // Send the password to the input.
+        WebElement logInButton = logInModal.findElement(By.xpath(LOGIN_BUTTON_LOCATOR)); // Find the LogIn button;
+        logInButton.click();                                                             // Click on the button.
+
+        System.out.println("LOGGED IN WITH CREDENTIALS:");
+        System.out.println("  Logged with Username: " + TestData.getUniqueUsernameGC());
+        System.out.println("  Logged with Password: " + PASSWORD);
+        System.out.println();
+
+        WebElement logOutButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(LOGOUT_BUTTON_LOCATOR)));
+        logOutButton.click();
+
+        HomePage homePageAfterLogout = new HomePage(driver); // New instance of the Home Page after logs out;
+
+        SoftAssert softAssert = new SoftAssert();                   // Add Soft Assertion (too many assertions);
+        softAssert.assertTrue(homePageAfterLogout.isHeaderBrandDisplayed(), "Header Brand is missing");
+        softAssert.assertTrue(homePageAfterLogout.isHeaderLogoDisplayed(), "Header Logo is missing");
+        softAssert.assertTrue(homePageAfterLogout.isNavBarDisplayed(), "Nav Bar is missing");
+        softAssert.assertTrue(homePageAfterLogout.isNavButtonHomeDisplayed(), "Nav Button Home is missing");
+        softAssert.assertTrue(homePageAfterLogout.isNavButtonContactDisplayed(), "Nav Button Contact is missing");
+        softAssert.assertTrue(homePageAfterLogout.isNavButtonAboutUsDisplayed(), "Nav Button About Us is missing");
+        softAssert.assertTrue(homePageAfterLogout.isNavButtonCartDisplayed(), "Nav Button Cart is missing");
+        softAssert.assertTrue(homePageAfterLogout.isNavButtonLogInDisplayed(), "Nav Button Log In is missing");
+        softAssert.assertTrue(homePageAfterLogout.isNavButtonSignUpDisplayed(), "Nav Button Sign Up is missing");
+        softAssert.assertAll();
+    }
+
+
 }
