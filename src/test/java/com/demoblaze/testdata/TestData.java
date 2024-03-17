3d914967ec35e6096eae8d7ff9aec7f9b9f5c2af
Andriy Glabay <rishelevsky@gmail.com>
2024-03-18 00:16:28 +0200

    TestData.java: create vars for simulate wrong username and wrong password

--- src/test/java/com/demoblaze/testdata/TestData.java
+++ src/test/java/com/demoblaze/testdata/TestData.java
@@ -7,6 +7,7 @@ public class TestData {
     public static final String USERNAME_3 = "WQ8T1HyUUD"; // Generated with www.random.org
     public static final String USERNAME_4 = "ESpc8dds9Y"; // Generated with www.random.org
     public static final String USERNAME_5 = "kWFUYNaAlc"; // Generated with www.random.org
+    public static final String USERNAME_WRONG = "WrongUsername";
 
     public static final String PASSWORD = "Test-12345";
     public static final String PASSWORD_1 = "Test-123abc";
@@ -14,6 +15,7 @@ public class TestData {
     public static final String PASSWORD_3 = "123581321";
     public static final String PASSWORD_4 = "R0S0T8wZHc"; // Generated with www.random.org
     public static final String PASSWORD_5 = "vIq3YCdNxi"; // Generated with www.random.org
+    public static final String PASSWORD_WRONG = "WrongPassword";
 
     public static String uniqueUsernameGC;
     public static String getUniqueUsernameGC() {
