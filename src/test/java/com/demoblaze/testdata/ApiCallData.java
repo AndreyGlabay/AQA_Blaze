541eb9ad49102108a095176b631de6725974f8a1
Andriy Glabay <rishelevsky@gmail.com>
2024-03-19 23:00:34 +0200

    ApiCallData.java: add test data for API test (add Items ID=4 and ID=5 to the cart)

--- src/test/java/com/demoblaze/testdata/ApiCallData.java
+++ src/test/java/com/demoblaze/testdata/ApiCallData.java
@@ -1,2 +1,10 @@
-package com.demoblaze.testdata;public class ApiCallData {
+package com.demoblaze.testdata;
+
+public class ApiCallData {
+    public static final String COOKIE = "R0MtdXNlci0yMDI0MDMxOTE1MTA1MzE3MTE0NTM=";
+    public static final String ID_4 = "6bbeed25-5145-c6f1-431d-80f9cca5793e";
+    public static final String ID_5 = "1001ef84-083a-9f30-4e6c-dba3362edb6c";
+    public static final int PROD_ID_4 = 4;
+    public static final int PROD_ID_5 = 5;
+    public static final boolean FLAG = true;
 }
