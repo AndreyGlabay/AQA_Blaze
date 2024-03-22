b51f5ad06f24e9fd9a97715076e35840895b7699
Andriy Glabay <rishelevsky@gmail.com>
2024-03-22 22:23:24 +0200

    TestData.java: (1) add constants for Grid URL and Base URL; (2) add comments

--- src/test/java/com/demoblaze/testdata/TestData.java
+++ src/test/java/com/demoblaze/testdata/TestData.java
@@ -1,6 +1,11 @@
 package com.demoblaze.testdata;
 
 public class TestData {
+    // URL CONSTANTS
+    public static final String GRID_URL = "http://192.168.0.102:4444";
+    public static final String BASE_URL = "https://demoblaze.com/";
+
+    // USERNAME CONSTANTS
     public static final String USERNAME = "JohnJohnson";
     public static final String USERNAME_1 = "timhce4eNP"; // Generated with www.random.org
     public static final String USERNAME_2 = "x81DfDaelO"; // Generated with www.random.org
@@ -9,6 +14,7 @@ public class TestData {
     public static final String USERNAME_5 = "kWFUYNaAlc"; // Generated with www.random.org
     public static final String USERNAME_WRONG = "WrongUsername";
 
+    //PASSWORD CONSTANTS
     public static final String PASSWORD = "Test-12345";
     public static final String PASSWORD_1 = "Test-123abc";
     public static final String PASSWORD_2 = "Test-abcde";
@@ -17,6 +23,7 @@ public class TestData {
     public static final String PASSWORD_5 = "vIq3YCdNxi"; // Generated with www.random.org
     public static final String PASSWORD_WRONG = "WrongPassword";
 
+    // VAR AND ACCESSORS FOR UNIQUE USERNAME VALUE (USING TIMESTAMP)
     public static String uniqueUsernameGC;
     public static String getUniqueUsernameGC() {
         return uniqueUsernameGC;
@@ -24,5 +31,4 @@ public class TestData {
     public static void setUniqueUsernameGC(String username) {
         uniqueUsernameGC = username;
     }
-
 }
