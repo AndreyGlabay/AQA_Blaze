24552df8ac00f3acbe648785b5af702ab97026f4
Andriy Glabay <rishelevsky@gmail.com>
2024-03-25 15:19:33 +0200

    Project: add ProgectPage page object

---
+++ src/test/java/com/demoblaze/pageobject/ProductPage.java
@@ -0,0 +1,9 @@
+package com.demoblaze.pageobject;
+
+public class ProductId4 {
+    public static final String PRODUCT4_URL = "https://demoblaze.com/prod.html?idp_=4";
+    public static final String PRODUCT4_TITLE = "//*[@id=\"tbodyid\"]/h2";
+    public static final String PRODUCT4_PRICE = "//*[@id=\"tbodyid\"]/h3";
+    public static final String PRODUCT4_DESCRIPTION = "//*[@id=\"more-information\"]/p";
+    public static final String ADD_TO_CART_BUTTON = "//*[@id=\"tbodyid\"]/div[2]/div/a";
+}
