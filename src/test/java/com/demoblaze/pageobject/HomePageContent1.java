4461e2dce210b93ca909e03d7cd630952688ce8d
Andriy Glabay <rishelevsky@gmail.com>
2024-03-25 04:51:08 +0200

    HomePageContent1.java & HomePageContent2.java: create page objects for the home page products links

--- src/test/java/com/demoblaze/pageobject/HomePageContent1.java
+++ src/test/java/com/demoblaze/pageobject/HomePageContent1.java
@@ -1,2 +1,78 @@
-package com.demoblaze.pageobject;public class HomePageContentPage1 {
+package com.demoblaze.pageobject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePageContent1 {
+    @FindBy(xpath = "//a[text()='Samsung galaxy s6']")
+    private WebElement linkProductId1;
+
+    @FindBy(xpath = "//a[text()='Nokia lumia 1520']")
+    private WebElement linkProductId2;
+
+    @FindBy(xpath = "//a[text()='Nexus 6']")
+    private WebElement linkProductId3;
+
+    @FindBy(xpath = "//a[text()='Samsung galaxy s7']")
+    private WebElement linkProductId4;
+
+    @FindBy(xpath = "//a[text()='Iphone 6 32gb']")
+    private WebElement linkProductId5;
+
+    @FindBy(xpath = "//a[text()='Sony xperia z5']")
+    private WebElement linkProductId6;
+
+    @FindBy(xpath = "//a[text()='HTC One M9']")
+    private WebElement linkProductId7;
+
+    @FindBy(xpath = "//a[text()='Sony vaio i5']")
+    private WebElement linkProductId8;
+
+    @FindBy(xpath = "//a[text()='Sony vaio i7']")
+    private WebElement linkProductId9;
+
+    // Constructor
+    public HomePageContent1(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    // Methods to check if links are displayed
+    public boolean isLinkProductId1displayed() {
+        return linkProductId1.isDisplayed();
+    }
+
+    public boolean isLinkProductId2displayed() {
+        return linkProductId2.isDisplayed();
+    }
+
+    public boolean isLinkProductId3displayed() {
+        return linkProductId3.isDisplayed();
+    }
+
+    public boolean isLinkProductId4displayed() {
+        return linkProductId4.isDisplayed();
+    }
+
+    public boolean isLinkProductId5displayed() {
+        return linkProductId5.isDisplayed();
+    }
+
+    public boolean isLinkProductId6displayed() {
+        return linkProductId6.isDisplayed();
+    }
+
+    public boolean isLinkProductId7displayed() {
+        return linkProductId7.isDisplayed();
+    }
+
+    public boolean isLinkProductId8displayed() {
+        return linkProductId8.isDisplayed();
+    }
+
+    public boolean isLinkProductId9displayed() {
+        return linkProductId9.isDisplayed();
+    }
 }
--- src/test/java/com/demoblaze/pageobject/HomePageContent2.java
+++ src/test/java/com/demoblaze/pageobject/HomePageContent2.java
@@ -1,2 +1,55 @@
-package com.demoblaze.pageobject;public class HomePageContent2 {
+package com.demoblaze.pageobject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePageContent2 {
+    @FindBy(css = "a[href='prod.html?idp_=10']")
+    private WebElement linkProductId10;
+
+    @FindBy(css = "a[href='prod.html?idp_=11']")
+    private WebElement linkProductId11;
+
+    @FindBy(css = "a[href='prod.html?idp_=12']")
+    private WebElement linkProductId12;
+
+    @FindBy(css = "a[href='prod.html?idp_=13']")
+    private WebElement linkProductId13;
+
+    @FindBy(css = "a[href='prod.html?idp_=14']")
+    private WebElement linkProductId14;
+
+    @FindBy(css = "a[href='prod.html?idp_=15']")
+    private WebElement linkProductId15;
+
+    public HomePageContent2(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public boolean isLinkProductId10displayed() {
+        return linkProductId10.isDisplayed();
+    }
+
+    public boolean isLinkProductId11displayed() {
+        return linkProductId11.isDisplayed();
+    }
+
+    public boolean isLinkProductId12displayed() {
+        return linkProductId12.isDisplayed();
+    }
+
+    public boolean isLinkProductId13displayed() {
+        return linkProductId13.isDisplayed();
+    }
+
+    public boolean isLinkProductId14displayed() {
+        return linkProductId14.isDisplayed();
+    }
+
+    public boolean isLinkProductId15displayed() {
+        return linkProductId15.isDisplayed();
+    }
 }
