6cda7ba04b4fa547d345fb00ec10c33199636100
Andriy Glabay <rishelevsky@gmail.com>
2024-03-25 16:49:31 +0200

    OrderCompleteModal.java: add constants for the page object

--- src/test/java/com/demoblaze/pageobject/OrderCompleteModal.java
+++ src/test/java/com/demoblaze/pageobject/OrderCompleteModal.java
@@ -1,2 +1,9 @@
-package com.demoblaze.pageobject;public class OrderCompleteModal {
+package com.demoblaze.pageobject;
+
+public class OrderCompleteModal {
+    public static final String ORDER_COMPLETE_MODAL = "/html/body/div[10]";
+    public static final String ORDER_COMPLETE_SIGN = "/html/body/div[10]/div[4]/div[1]";
+    public static final String ORDER_COMPLETE_THANKS = "/html/body/div[10]/h2";
+    public static final String ORDER_COMPLETE_DATA = "/html/body/div[10]/p";
+    public static final String ORDER_COMPLETE_OK_BUTTON = "/html/body/div[10]/div[7]/div/button";
 }
