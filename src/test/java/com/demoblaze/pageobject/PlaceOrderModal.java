fb98837cd660ea77e494e59d8b5aa93a8e8bf403
Andriy Glabay <rishelevsky@gmail.com>
2024-03-25 16:38:16 +0200

    PlaceOrderModal.java: add constants

--- src/test/java/com/demoblaze/pageobject/PlaceOrderModal.java
+++ src/test/java/com/demoblaze/pageobject/PlaceOrderModal.java
@@ -1,2 +1,16 @@
-package com.demoblaze.pageobject;public class PlaceOrderModal {
+package com.demoblaze.pageobject;
+
+public class PlaceOrderModal {
+    public static final String PLACE_ORDER_MODAL = "//*[@id=\"orderModal\"]/div/div";
+    public static final String PLACE_ORDER_TITLE = "//*[@id=\"orderModalLabel\"]";
+    public static final String PLACE_ORDER_X_BUTTON = "//*[@id=\"orderModal\"]/div/div/div[1]/button";
+    public static final String PLACE_ORDER_TOTAL_PRICE = "//*[@id=\"totalm\"]";
+    public static final String PLACE_ORDER_NAME_INPUT = "//*[@id=\"name\"]";
+    public static final String PLACE_ORDER_COUNTRY_INPUT = "//*[@id=\"country\"]";
+    public static final String PLACE_ORDER_CITY_INPUT = "//*[@id=\"city\"]";
+    public static final String PLACE_ORDER_CREDIT_CARD_INPUT = "//*[@id=\"card\"]";
+    public static final String PLACE_ORDER_MONTH_INPUT = "//*[@id=\"month\"]";
+    public static final String PLACE_ORDER_YEAR_INPUT = "//*[@id=\"year\"]";
+    public static final String PLACE_ORDER_CLOSE_BUTTON = "//*[@id=\"orderModal\"]/div/div/div[3]/button[1]";
+    public static final String PLACE_ORDER_PURCHASE_BUTTON = "//*[@id=\"orderModal\"]/div/div/div[3]/button[2]";
 }
