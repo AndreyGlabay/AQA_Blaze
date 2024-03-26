7be74f82517047f900b20cf4e6b7f0d3e9d5cfd1
Andriy Glabay <rishelevsky@gmail.com>
2024-03-26 02:36:17 +0200

    CartPage.java: add method for add product to the cart

--- src/test/java/com/demoblaze/pageobject/CartPage.java
+++ src/test/java/com/demoblaze/pageobject/CartPage.java
@@ -1,5 +1,8 @@
 package com.demoblaze.pageobject;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class CartPage {
     public static final String TABLE_TITLE = "//*[@id=\"page-wrapper\"]/div/div[1]/h2";
 
@@ -27,5 +30,29 @@ public class CartPage {
     public static final String TOTAL_TITLE = "//*[@id=\"page-wrapper\"]/div/div[1]/h2";
     public static final String TOTAL_VALUE = "//*[@id=\"totalp\"]";
     public static final String PLACE_ORDER_BUTTON = "//*[@id=\"page-wrapper\"]/div/div[2]/button";
+
+    private final WebDriver driver;
+
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public boolean isProductAddedToCart(String productName, String productPrice) {
+        // Construct the XPath for the product title and price in the cart table
+        String productTitleXPath = "//*[text()='" + productName + "']";
+        String productPriceXPath = "//*[text()='" + productPrice + "']";
+
+        // Check if both the product title and price elements are present in the cart table
+        return !driver.findElements(By.xpath(ROW_1_TITLE)).isEmpty() &&
+                !driver.findElements(By.xpath(ROW_1_PRICE)).isEmpty();
+    }
+
+    public String getTotalValue() {
+        return driver.findElement(By.xpath(TOTAL_VALUE)).getText();
+    }
+
+    public void placeOrder() {
+        driver.findElement(By.xpath(PLACE_ORDER_BUTTON)).click();
+    }
 
 }
