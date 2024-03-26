7d3340a42d39a26bb131ad102fe71e3f28a37f0d
Andriy Glabay <rishelevsky@gmail.com>
2024-03-26 16:01:32 +0200

    ProductPage.java: add to the constructor wait for elements appearance

--- src/test/java/com/demoblaze/pageobject/ProductPage.java
+++ src/test/java/com/demoblaze/pageobject/ProductPage.java
@@ -3,6 +3,10 @@ package com.demoblaze.pageobject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ProductPage {
     public static final String PRODUCT_TITLE = "//*[@id=\"tbodyid\"]/h2";
@@ -10,13 +14,19 @@ public class ProductPage {
     public static final String PRODUCT_DESCRIPTION = "//*[@id=\"more-information\"]/p";
     public static final String ADD_TO_CART_BUTTON = "//*[@id=\"tbodyid\"]/div[2]/div/a";
 
-    private final WebElement productTitle;
-    private final WebElement productPrice;
-    private final WebElement productDescription;
-    private final WebElement addToCartButton;
+    private WebElement productTitle;
+    private WebElement productPrice;
+    private WebElement productDescription;
+    private WebElement addToCartButton;
 
     // PRODUCT PAGE
     public ProductPage(WebDriver driver) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(1000));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(PRODUCT_TITLE)));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(PRODUCT_PRICE)));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(PRODUCT_DESCRIPTION)));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(ADD_TO_CART_BUTTON)));
+
         productTitle = driver.findElement(By.xpath(PRODUCT_TITLE));;
         productPrice = driver.findElement(By.xpath(PRODUCT_PRICE));;
         productDescription = driver.findElement(By.xpath(PRODUCT_DESCRIPTION));;
