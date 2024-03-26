bb2b407f8d51772ac1593d6fa5cba708f1370d2b
Andriy Glabay <rishelevsky@gmail.com>
2024-03-26 03:41:13 +0200

    ProductPropertiesReader.java: create Product Properties Reader for parse data from prduct.properties

--- src/test/java/com/demoblaze/utilities/ProductPropertiesReader.java
+++ src/test/java/com/demoblaze/utilities/ProductPropertiesReader.java
@@ -1,2 +1,28 @@
-package com.demoblaze.utilities;public class ProductPropertiesReader {
+package com.demoblaze.utilities;
+
+import com.demoblaze.testdata.ProductData;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class ProductPropertiesReader {
+    public static Map<String, ProductData> readProductProperties() throws IOException {
+        Properties properties = new Properties();
+        InputStream inputStream = ProductPropertiesReader.class.getClassLoader().getResourceAsStream("product.properties");
+        properties.load(inputStream);
+
+        Map<String, ProductData> productDataMap = new HashMap<>();
+        for (int i = 1; i <= 15; i++) {
+            String productId = "PRODUCT_" + i;
+            String title = properties.getProperty(productId + "_TITLE");
+            String price = properties.getProperty(productId + "_PRICE");
+            String description = properties.getProperty(productId + "_DESCRIPTION");
+            productDataMap.put(productId, new ProductData(title, price, description));
+        }
+        return productDataMap;
+    }
 }
