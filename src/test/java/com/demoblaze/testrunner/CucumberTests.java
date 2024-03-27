6110c812ae7df9db58883a154d923afe6579f897
Andriy Glabay <rishelevsky@gmail.com>
2024-03-27 22:38:40 +0200

    CucumberTests.java: Create cucumber test runner

--- src/test/java/com/demoblaze/testrunner/CucumberTests.java
+++ src/test/java/com/demoblaze/testrunner/CucumberTests.java
@@ -1,15 +1,15 @@
 package com.demoblaze.testrunner;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.testng.CucumberOptions;
 import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources"},
-        glue = "com.demoblaze.scenarios",
-        plugin = {"testng:testng.xml"}
+        features = "src/test/resources", // Features path;
+        glue = {"com.demoblaze.scenarios"}, // Step Definitions path;
+        plugin = {"pretty", "html:target/cucumber-reports"}
 )
 
-public class CucumberTestRunner {
+public class CucumberTests {
 }
