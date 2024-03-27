1ac5eb651136f95d92f605f4313e15706b28dfbc
Andriy Glabay <rishelevsky@gmail.com>
2024-03-27 04:02:39 +0200

    PlaceOrderPropertiesReader.java: implement the reader for users data from placeorder.properties

--- src/test/java/com/demoblaze/utilities/PlaceOrderPropertiesReader.java
+++ src/test/java/com/demoblaze/utilities/PlaceOrderPropertiesReader.java
@@ -1,2 +1,16 @@
-package com.demoblaze.utilities;public class PlaceOrderPropertiesReader {
+package com.demoblaze.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PlaceOrderPropertiesReader {
+
+    public static Properties readPropertiesFile(String filePath) throws IOException, IOException {
+        Properties properties = new Properties();
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+        properties.load(fileInputStream);
+        fileInputStream.close();
+        return properties;
+    }
 }
