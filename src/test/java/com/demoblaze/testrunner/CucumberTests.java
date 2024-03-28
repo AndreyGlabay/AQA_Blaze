d44befc7a6aff2732143e4e3a74f6913875ba91b
Andriy Glabay <rishelevsky@gmail.com>
2024-03-28 13:32:44 +0200

    CucumberTests.java: add pligin 'io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm' and @Test annotation

--- src/test/java/com/demoblaze/testrunner/CucumberTests.java
+++ src/test/java/com/demoblaze/testrunner/CucumberTests.java
@@ -1,15 +1,18 @@
 package com.demoblaze.testrunner;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.testng.annotations.Test;
 
 @RunWith(Cucumber.class)
+@Test
 @CucumberOptions(
         features = "src/test/resources", // Features path;
         glue = {"com.demoblaze.scenarios"}, // Step Definitions path;
-        plugin = {"pretty", "html:target/cucumber-reports"}
+        plugin = {"pretty", "html:target/cucumber-reports", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
 
-public class CucumberTests {
+public class CucumberTests extends AbstractTestNGCucumberTests {
 }
